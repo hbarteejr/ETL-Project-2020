dbc18fc92d7c8a5f57d1ebdc09d6fc3e6a89aa02
Howard Bartee, Jr <60280558+hbarteejr@users.noreply.github.com>
2020-04-27 19:37:28 -0500

    Add files via upload

--- NFLDRAFT20092019.sql
+++ NFLDRAFT20092019.sql
@@ -1,15 +1,11 @@
-DROP TABLE IF EXISTS draft_year;
 DROP TABLE IF EXISTS round;
-DROP TABLE IF EXISTS player;
-DROP TABLE IF EXISTS position;
-DROP TABLE IF EXISTS conference;
+
 
 
 CREATE TABLE round (
-  draft_year character varying(45) NOT NULL,
-  round character varying(45) DEFAULT 1 NOT NULL,
+  draft year character varying(45) NOT NULL,
+  round character varying(45) NOT NULL,
   player character varying(45) NOT NULL,
   position character varying(45) NOT NULL,
   conference character varying(45) NOT NULL
 );
-
